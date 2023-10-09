ccbfb6dcf9f852ed39cb5668c3d5af000cb8ac53
SFState <vishalsharma1907@gmail.com>
2023-10-08 19:44:37 -0700

    Refactor package.json and app.ts, remove users.route.ts, and update server.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,11 +11,14 @@ class App {
   public app: Express;
   public env: string;
   public port: string | number;
+
   constructor(routes: Routes[]) {
+    // Create an Express application
     this.app = express();
+
+    // Get environment, port, and initialize routes and error handling
     this.env = NODE_ENV || 'development';
     this.port = PORT;
-
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
@@ -26,19 +29,25 @@ class App {
   }
 
   private initializeMiddlewares() {
-    this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(compression());
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
+    // Set up middleware functions
+    this.app.use(morgan(LOG_FORMAT, { stream })); // Logging with Morgan
+    this.app.use(compression()); // Gzip compression
+    this.app.use(express.json()); // Parse JSON requests
+    this.app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+
+    // Serve static files in production mode
     if (this.env === 'production') {
       this.app.use(express.static(path.join(__dirname, '../build')));
     }
   }
 
   private initializeRoutes(routes: Routes[]) {
+    // Configure routes for the application
     routes.forEach(route => {
-      this.app.use('/api/v1/', route.router);
+      this.app.use('/api/v1/', route.router); // Prefix routes with '/api/v1/'
     });
+
+    // Serve the React app's HTML file in production
     if (this.env === 'production') {
       this.app.get('/', this.homePageHandler);
     } else {
@@ -50,10 +59,12 @@ class App {
   }
 
   private homePageHandler(req: Request, res: Response) {
+    // Serve the HTML file for the home page in production
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   }
 
   private initializeErrorHandling() {
+    // Configure error handling middleware
     this.app.use(errorMiddleware);
   }
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,16 +6,20 @@ import { Server } from 'socket.io';
 import JSONPlaceholderRoute from './routes/jsonplaceholder.route';
 validateEnv();
 
+// Create an instance of the Express application and specify the routes to use
 const app = new App([new IndexRoute(), new JSONPlaceholderRoute()]);
 
+// Create an HTTP server using the Express application
 const server = http.createServer(app.getServer());
 
+// Create a Socket.IO server and configure it with CORS settings
 export const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    credentials: true,
+    origin: '*', // Allow requests from any origin (you may want to restrict this in production)
+    methods: ['GET', 'POST'], // Allow specified HTTP methods
+    credentials: true, // Enable credentials (e.g., cookies) for cross-origin requests
   },
 });
 
+// Start the server by listening on the specified port
 server.listen(app.port, () => console.log(`Server running on port ${app.port}`));
